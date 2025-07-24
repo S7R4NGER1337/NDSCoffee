b247ed8edc97956c700228e4eb1500fb23a53a8c
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-24 15:27:25 +0300

    adding functionality to product page

--- client/src/product/ProductPage.js
+++ client/src/product/ProductPage.js
@@ -1,9 +1,30 @@
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
+async function getProductData(id) {
+  try {
+    const response = await fetch(`http://localhost:3030/products/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
 
-export default function ProductPage(){
-    const productId = useLocation().pathname.split('/')[2]
-    console.log(productId);
-    
-    return <h1>Hello</h1>
+export default function ProductPage() {
+  const [productData, setProductData] = useState({});
+  const productId = useLocation().pathname.split("/")[2];
+
+  useEffect(() => {
+    async function setData() {
+      setProductData(await getProductData(productId));
+    }
+    setData();
+  }, []);
+
+  return (
+    <>
+      <h1>{productData.name}</h1>
+    </>
+  );
 }
