35f14bbb6267cf4de964efe8f282651435862375
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-24 16:41:12 +0300

    rendering product image in featured products

--- client/src/home/FeaturedProduct.js
+++ client/src/home/FeaturedProduct.js
@@ -4,7 +4,7 @@ export default function FeaturedProduct({productData}) {
     
     return (
         <div className="featuredProduct">
-            <img alt="productImage" src="logo192.png" className="featuredProductImg"/>
+            <img alt="productImage" src={productData.image} className="featuredProductImg"/>
             <h1 className="featuredProductName">{productData.name}</h1>
             <button className="featuredProductBtn">View Product</button>
         </div>
