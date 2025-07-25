551324bf3583676fe12ac5c0f08e5cafd9f3d4bd
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-25 19:27:48 +0300

    working with user data before submitting an order

--- client/src/order/Order.js
+++ client/src/order/Order.js
@@ -1,13 +1,35 @@
 import OrderCard from "./OrderCard"
+import { useState } from "react"
+import './order.css'
 
 export default function Order() {
+    const [userData, setUserData] = useState({
+        name: '',
+        address: '',
+        phone: ''
+    })
     const cart = JSON.parse(localStorage.getItem('cart'))
 
-    
+    function submitOrder(params) {
+        console.log(userData);
+        
+    }
     return (
         <>
         <div className="userData">
+            <form className="orderForm">
 
+                <label> Name </label>
+                <input required type="text" name="name" value={userData.name} onChange={(e) => setUserData({...userData, name: e.target.value})}/>
+
+                <label> Address </label>
+                <input required type="text" name="address" value={userData.address} onChange={(e) => setUserData({...userData, address: e.target.value})}/>
+
+                <label> Phone </label>
+                <input required type="text" name="phone" value={userData.phone} onChange={(e) => setUserData({...userData, phone: e.target.value})}/>
+
+                <button type="button" onClick={() => submitOrder()}>ORDER NOW</button>
+            </form>
         </div>
         <div className="cart">
             {cart.map((item) => <OrderCard data={item} key={item.id}/>)}
--- client/src/order/OrderCard.js
+++ client/src/order/OrderCard.js
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
 import "./orderCard.css";
 
-
-function methodsQty(productId, operation) {
-  const cartInfo = JSON.parse(localStorage.getItem("cart")) || [];
-  const index = cartInfo.findIndex((item) => item.id === productId);
-
-  if(operation === 'sub'){
-    cartInfo[index].qty -= 1;
-  }
-  if(operation === 'sum'){
-    cartInfo[index].qty += 1
-  }
-  
-  if(cartInfo[index].qty <= 0){
-    cartInfo.splice(cartInfo.indexOf(cartInfo[index]), 1)
-  }
-  localStorage.setItem("cart", JSON.stringify(cartInfo));
-  return cartInfo;
-}
-
-
 export default function OrderCard({ data }) {
   const [product, setProduct] = useState([]);
+  const [qty, setQty] = useState(0)
+
+  function methodsQty(productId, operation) {
+    const cartInfo = JSON.parse(localStorage.getItem("cart")) || [];
+    const index = cartInfo.findIndex((item) => item.id === productId);
+
+    if (operation === "sub") {
+      cartInfo[index].qty -= 1;
+      setQty(cartInfo[index].qty)
+    }
+    if (operation === "sum") {
+      cartInfo[index].qty += 1;
+      setQty(cartInfo[index].qty)
+    }
+
+    if (qty-1 < 1) {
+      cartInfo.splice(cartInfo.indexOf(cartInfo[index]), 1);
+    }
+    localStorage.setItem("cart", JSON.stringify(cartInfo));
+    return cartInfo;
+  }
 
   useEffect(() => {
     async function getProduct() {
@@ -41,14 +42,19 @@ export default function OrderCard({ data }) {
     getProduct();
   }, []);
 
+  useEffect(() =>{
+    setQty(data.qty)
+  },[data.qty])
+
   return (
     <div className="cardContainer">
       <img alt="cardImage" src={product.image} className="cardImage" />
       <div>
         <h1>{product.name}</h1>
         <p>{product.price * data.qty} лв</p>
-        <p>{data.qty}</p>
-        <button onClick={() => methodsQty(data.id, 'sub')}> - </button>
+        <p>{qty}</p>
+        <button onClick={() => methodsQty(data.id, "sub")}> - </button>
+        <button onClick={() => methodsQty(data.id, "sum")}> + </button>
       </div>
     </div>
   );
