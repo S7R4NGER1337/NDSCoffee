da8e8b8f6bfd27d0eb97d6c15999eae7fc30fe69
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-25 20:16:57 +0300

    Editing type of product

--- server/models/Order.js
+++ server/models/Order.js
@@ -4,7 +4,7 @@ const OrderSchema = new mongoose.Schema({
     name: String,
     address: String,
     phone: String,
-    productId: String
+    productId: Array
 })
 
 const Order = mongoose.model('Order', OrderSchema)
