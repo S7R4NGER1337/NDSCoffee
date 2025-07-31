9f1d727d608f9569e41b951b093d9eda41aa8847
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-31 16:22:34 +0300

    adding some more styles

--- client/src/catalog/Catalog.js
+++ client/src/catalog/Catalog.js
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
 import CatalogCard from "./CatalogCard";
 import styles from './catalog.module.css'
+import Nav from "../components/nav";
 
 export default function Catalog() {
   const [products, setProducts] = useState([]);
+  const linkings = [
+    {
+      linkPath: '/',
+      linkName: 'Home'
+    },
+    {
+      linkPath: "/catalog",
+      linkName: "Catalog",
+    },
+  ];
 
   useEffect(() => {
     async function getProducts() {
@@ -16,10 +27,11 @@ export default function Catalog() {
 
   return (
     <>
-      <h1>Catalog</h1>
+    <Nav navName="NDS" navLinks={linkings}/>
+      <h1 className={styles.catalogHeading}>Catalog</h1>
       <div className={styles.catalogCards}>
         {products.map((product) => (
-          <CatalogCard productData={product} key={product._id} />
+          <CatalogCard productData={product} key={product._id}/>
         ))}
       </div>
     </>
