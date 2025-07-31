7d8b098be2150e552df3f71caedc8128c3a98fc7
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-31 16:28:18 +0300

    fixing error with price in order page

--- client/src/order/OrderCard.js
+++ client/src/order/OrderCard.js
@@ -4,6 +4,7 @@ import styles from "./orderCard.module.css";
 export default function OrderCard({ data }) {
   const [product, setProduct] = useState([]);
   const [qty, setQty] = useState(0)
+  const [price, setPrice] = useState(0)
 
   function methodsQty(productId, operation) {
     const cartInfo = JSON.parse(localStorage.getItem("cart")) || [];
@@ -11,10 +12,14 @@ export default function OrderCard({ data }) {
 
     if (operation === "sub") {
       cartInfo[index].qty -= 1;
+
+      setPrice(cartInfo[index].qty * product.price)
       setQty(cartInfo[index].qty)
     }
     if (operation === "sum") {
       cartInfo[index].qty += 1;
+
+      setPrice(cartInfo[index].qty * product.price)
       setQty(cartInfo[index].qty)
     }
 
@@ -42,16 +47,17 @@ export default function OrderCard({ data }) {
     getProduct();
   }, [data.id]);
 
-  useEffect(() =>{
+  useEffect(() =>{    
+    setPrice(data.qty * product.price)
     setQty(data.qty)
-  },[data.qty])
+  },[data.qty, product])
 
   return (
     <div className={styles.cardContainer}>
       <img alt="cardImage" src={product.image} className={styles.cardImage} />
       <div>
         <h1>{product.name}</h1>
-        <p>{product.price * data.qty} лв</p>
+        <p>{price} лв</p>
         <p>{qty}</p>
         <button onClick={() => methodsQty(data.id, "sub")}> - </button>
         <button onClick={() => methodsQty(data.id, "sum")}> + </button>
