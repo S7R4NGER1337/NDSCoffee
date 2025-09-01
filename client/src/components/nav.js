f020b23ba4a66aea0a930441cf9f300e9e716192
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-01 17:04:13 +0300

    changing how navigation looks

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -8,19 +8,40 @@ import styles from './nav.module.css'
 
 export default function Nav(navData) {
 
+  const links = [
+    {
+      linkPath: '/',
+      linkName: 'Home'
+    },
+    {
+      linkPath: '/catalog',
+      linkName: 'Coffee Beans'
+    },
+    {
+      linkPath: '/',
+      linkName: 'About Us'
+    },
+  ]
   
   return(
-    <div className={styles.navigation} style={{color: navData.color}}>
-      <h1 className={styles.navName}>{navData.navName}</h1>
-      <div className={styles.navLinks}>
-          {navData['navLinks'].map(link =>
-            link.linkPath !== '/about' ?
-            <Link to={link.linkPath} style={{ textDecoration: 'none', color: 'inherit' }} key={link.linkPath}>
+      <div className={styles.navigation}>
+        <div className={styles.navInfo}>
+          <img src='/favicon.ico' alt='icon' className={styles.navIcon}/>
+          <h1 className={styles.navName}>NDS Coffee</h1>
+        </div>
+
+        <div className={styles.navLinks}>
+          {links.map(link => 
+            <Link to={link.linkPath} style={{ textDecoration: 'none', color: 'inherit' }} key={link.linkName}>
               <p className={styles.navLink}>{link.linkName}</p>
-            </Link> : 
-            <p className={styles.navLink} onClick={() => navData.aboutRef.current?.scrollIntoView({ behavior: 'smooth' })} key={link.linkPath}> About </p>
+           </Link>
           )}
+        </div>
+
+        <div className={styles.navButtons}>
+          <img src='/magnifying-glass-solid-full.svg' alt='cart' className={styles.navButton}/>
+          <img src='/cart-shopping-regular-full.svg' alt='cart' className={styles.navButton}/>
+        </div>
       </div>
-    </div>
   )
 }
