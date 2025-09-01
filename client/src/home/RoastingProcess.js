b0e4988df89822bb7b40fb29ed36cbc1598ab495
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-02 01:11:52 +0300

    adding step number on the top of every icon

--- client/src/home/RoastingProcess.js
+++ client/src/home/RoastingProcess.js
@@ -1,26 +1,59 @@
-import styles from './roastingProcess.module.css'
+import styles from "./roastingProcess.module.css";
 
 export default function RoastingProcess() {
-    return(
-        <div className={styles.roastingProcessContainer}>
-        <h1 className={styles.roastingProcessName}>Our Roasting Process</h1>
-        <div className={styles.roastingProcessSteps}>
-            <div className={styles.processStep}>
-                <img className={styles.processImage} src="/fire-solid-full.svg" alt='processStep' />
-                <h1 className={styles.processName}>First Crack</h1>
-                <p className={styles.processDesc}>We carefully listen for the 'first crack', a key indicator that the beans are beginning to develop their unique flavor profile.</p>
-            </div>
-            <div className={styles.processStep}>
-                <img className={styles.processImage} src="/hourglass-start-solid-full.svg" alt='processStep' />
-                <h1 className={styles.processName}>Development Time</h1>
-                <p className={styles.processDesc}>The time between the first and second crack is crucial. We meticulously control this phase to achieve the perfect balance of acidity and sweetness.</p>
-            </div>
-            <div className={styles.processStep}>
-                <img className={styles.processImage} src="/snowflake-regular-full.svg" alt='processStep' />
-                <h1 className={styles.processName}>Cooling Down</h1>
-                <p className={styles.processDesc}>Once the optimal roast is achieved, we rapidly cool the beans to lock in the flavors and halt the roasting process, ensuring a consistent taste.</p>
-            </div>
+  return (
+    <div className={styles.roastingProcessContainer}>
+      <h1 className={styles.roastingProcessName}>Our Roasting Process</h1>
+      <div className={styles.roastingProcessSteps}>
+        <div className={styles.processStep}>
+          <div className={styles.iconWrapper}>
+            <img
+              className={styles.processImage}
+              src="/fire-solid-full.svg"
+              alt="processStep"
+            />
+            <span className={styles.badge}>1</span>
+          </div>
+          <h1 className={styles.processName}>First Crack</h1>
+          <p className={styles.processDesc}>
+            We carefully listen for the 'first crack', a key indicator that the
+            beans are beginning to develop their unique flavor profile.
+          </p>
         </div>
+        <div className={styles.processStep}>
+          <div className={styles.iconWrapper}>
+            <img
+              className={styles.processImage}
+              src="/hourglass-start-solid-full.svg"
+              alt="processStep"
+            />
+            <span className={styles.badge}>2</span>
+          </div>
+          <h1 className={styles.processName}>Development Time</h1>
+          <p className={styles.processDesc}>
+            The time between the first and second crack is crucial. We
+            meticulously control this phase to achieve the perfect balance of
+            acidity and sweetness.
+          </p>
         </div>
-    )
+        <div className={styles.processStep}>
+          <div className={styles.iconWrapper}>
+            <img
+              className={styles.processImage}
+              src="/snowflake-regular-full.svg"
+              alt="processStep"
+            />
+            <span className={styles.badge}>3</span>
+          </div>
+
+          <h1 className={styles.processName}>Cooling Down</h1>
+          <p className={styles.processDesc}>
+            Once the optimal roast is achieved, we rapidly cool the beans to
+            lock in the flavors and halt the roasting process, ensuring a
+            consistent taste.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
