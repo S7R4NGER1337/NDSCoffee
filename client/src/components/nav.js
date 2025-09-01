5dd3f933ab427a81df5bb52fe797d47a8a376191
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-01 17:04:47 +0300

    deleting unused imports

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,8 +1,3 @@
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
-// import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import styles from './nav.module.css'
 
