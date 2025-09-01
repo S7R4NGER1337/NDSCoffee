5467c9c9c0390418cd01a668606da2100b82343b
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-01 19:39:01 +0300

    creating new about us page

--- client/src/home/AboutUs.js
+++ client/src/home/AboutUs.js
@@ -3,17 +3,28 @@ import styles from "./aboutUs.module.css";
 export default function AboutUs() {
   return (
     <div className={styles.aboutUsSection}>
-      <h1 className={styles.aboutUsHeader}>About us</h1>
       <div className={styles.aboutUsInfo}>
-        <img alt="aboutUs" src="aboutUs.png" className={styles.aboutUsImg} />
+        <h1 className={styles.aboutUsHeader}>
+          Our Story: A Passion for Perfection
+        </h1>
         <p className={styles.aboutUsText}>
-          At NDS, we believe every cup of coffee is an opportunity for delight.
-          Our mission is to deliver carefully selected, freshly roasted coffee
-          beans directly to your home. We are passionate about coffee and strive
-          to share that experience with every customer, offering not just a
-          product, but a ritual.
+          Bean & Brew was born from a simple yet profound love for coffee. We
+          believe a perfect cup of coffee is more than just a morning ritual;
+          it's an experience. Our journey began with a quest to find the world's
+          most exceptional coffee beans, leading us to build relationships with
+          dedicated farmers who share our commitment to quality and
+          sustainability. 
+        </p>
+        <p className={styles.aboutUsText}>
+          Our mission is to bring you the finest,
+          ethically-sourced beans, roasted to perfection. We are passionate
+          about preserving the unique flavors of each origin, ensuring that
+          every cup you brew is a true reflection of its terroir. From the farm
+          to your cup, we are dedicated to a process that respects both people
+          and the planet.
         </p>
       </div>
+      <img alt="aboutUs" src="coffeeFarmer.png" className={styles.aboutUsImg} />
     </div>
   );
 }
