2d4f783ebac0e8a3dd5eb48c0df63a3fbed3d7b7
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-01 17:39:31 +0300

    changing the hero section

--- client/src/home/HeroSection.js
+++ client/src/home/HeroSection.js
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
-import styles from './heroSection.module.css'
+import styles from "./heroSection.module.css";
 
 export default function HeroSection() {
   return (
-    <div className={styles.banner}>
-      <h1 className={styles.bannerName}>NDS Coffee</h1>
-      <p className={styles.bannerText}>
-        Carefully selected beans from the world's finest plantations.
-      </p>
-      <Link
-        to={"/catalog"}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
-        <button className={styles.bannerBtn}>Explore Catalog</button>
-      </Link>
+    <div className={styles.heroWrapper}>
+        <img src="/mainBanner.jpg" alt="banner" className={styles.banner} />
+      <div className={styles.heroSectionContainer}>
+        <h1 className={styles.heroName}>The Finest Coffee Beans, Delivered</h1>
+        <p className={styles.heroText}>
+          Experience the rich aroma and exquisite taste of our ethically
+          sourced, expertly roasted coffee beans.
+        </p>
+        <Link
+          to={"/catalog"}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <button className={styles.bannerBtn}>Shop Coffee Beans</button>
+        </Link>
+      </div>
     </div>
   );
 }
