e154cb0e777887b9621a12919ad4f94337f598c1
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-03 17:34:36 +0300

    update

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,34 +1,32 @@
-import AboutUs from "./AboutUs"
-import FeaturedProducts from "./FeaturedProducts"
-import { useEffect, useRef } from 'react';
+import AboutUs from "./AboutUs";
+import FeaturedProducts from "./FeaturedProducts";
+import { useEffect, useRef } from "react";
 import HeroSection from "./HeroSection";
 import Offer from "../components/Offer";
 import RoastingProcess from "./RoastingProcess";
 import { useLocation } from "react-router-dom";
 
-
 export default function Home() {
-  const aboutRef = useRef(null)
+  const aboutRef = useRef(null);
 
-  const location = useLocation()
-  const from = location.state?.from || 'direct'
+  const location = useLocation();
+  const from = location.state?.from || "direct";
 
   useEffect(() => {
-    if(from === '/about'){
-        aboutRef.current.scrollIntoView()
+    if (from === "/about") {
+      aboutRef.current.scrollIntoView({ behavior: "smooth", block: "start"});
     }
-  }, [from])
+  }, [from]);
 
-
-    return (
-        <div style={{display: "flex", flexDirection: 'column', gap: '4rem'}}>
-           <HeroSection />
-           <Offer />
-            <FeaturedProducts/>
-            <div ref={aboutRef}>
-                <AboutUs />
-            </div>
-            <RoastingProcess />
-        </div>
-    )
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "4rem" }}>
+      <HeroSection />
+      <Offer />
+      <FeaturedProducts />
+      <div ref={aboutRef}>
+        <AboutUs ref={aboutRef}/>
+      </div>
+      <RoastingProcess />
+    </div>
+  );
 }
