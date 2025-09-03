e83b6cac72893cc888c437c116aa6939df3a1683
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-03 18:01:07 +0300

    adding filters in catalog page

--- client/src/catalog/Catalog.js
+++ client/src/catalog/Catalog.js
@@ -1,20 +1,13 @@
 import { useEffect, useState } from "react";
 import CatalogCard from "./CatalogCard";
-import styles from './catalog.module.css'
-import Nav from "../components/nav";
+import styles from "./catalog.module.css";
 
 export default function Catalog() {
   const [products, setProducts] = useState([]);
-  const linkings = [
-    {
-      linkPath: '/',
-      linkName: 'Home'
-    },
-    {
-      linkPath: "/catalog",
-      linkName: "Catalog",
-    },
-  ];
+  const [filters, setFilters] = useState({
+    roastedLevel: "",
+    origin: "",
+  });
 
   useEffect(() => {
     async function getProducts() {
@@ -27,11 +20,65 @@ export default function Catalog() {
 
   return (
     <>
-    <Nav navName="NDS" navLinks={linkings}/>
-      <h1 className={styles.catalogHeading}>Catalog</h1>
+      <h1 className={styles.catalogHeading}>All Coffee Beans</h1>
+      <p className={styles.catalogText}>
+        Explore our curated collection of the finest coffee beans from around
+        the world.
+      </p>
+      <div className={styles.filtersContainer}>
+        <div className={styles.filters}>
+          <select
+            name="RoastLevel"
+            value={filters.roastedLevel}
+            onChange={(e) =>
+              setFilters({ ...filters, roastedLevel: e.target.value })
+            }
+          >
+            <option value="" disabled hidden>
+              Roasted Level
+            </option>
+            <option value="Light">Light</option>
+            <option value="Medium">Medium</option>
+            <option value="MediumDark">Medium-Dark</option>
+            <option value="Dark">Dark</option>
+          </select>
+          <select
+            name="Origin"
+            value={filters.origin}
+            onChange={(e) => setFilters({ ...filters, origin: e.target.value })}
+          >
+            <option value="" disabled hidden>
+              Origin
+            </option>
+            <option value="Colombia">Colombia</option>
+            <option value="Ethiopia">Ethiopia</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Guatemala">Guatemala</option>
+            <option value="Kenya">Kenya</option>
+            <option value="CostaRica">Costa Rica</option>
+            <option value="Indonesia">Indonesia</option>
+            <option value="India">India</option>
+            <option value="Honduras">Honduras</option>
+            <option value="Panama">Honduras</option>
+          </select>
+        </div>
+        <div className={styles.sort}>
+          <select
+            name="RoastLevel"
+            value={filters.roastedLevel}
+            onChange={(e) =>
+              setFilters({ ...filters, roastedLevel: e.target.value })
+            }
+          >
+            <option value="Featured">Featured</option>
+            <option value="PriceL">Price: low-to-high</option>
+            <option value="PriceH">Price: high-to-low</option>
+          </select>
+        </div>
+      </div>
       <div className={styles.catalogCards}>
         {products.map((product) => (
-          <CatalogCard productData={product} key={product._id}/>
+          <CatalogCard productData={product} key={product._id} />
         ))}
       </div>
     </>
