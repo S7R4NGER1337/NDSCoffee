353438ae96d80c1d79e8e1265fd74c7cfc23e196
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-03 15:43:22 +0300

    making offer link to work

--- client/src/components/Offer.js
+++ client/src/components/Offer.js
@@ -1,11 +1,17 @@
-import styles from './offer.module.css'
+import styles from "./offer.module.css";
+import { Link } from "react-router-dom";
 
 export default function Offer() {
-    return(
-        <div className={styles.offerContainer}>
-            <h1 className={styles.offerName}>Limited-Time Offer</h1>
-            <p className={styles.offerText}>Save 20% on our Premium Selection of Single Origin Coffee Beans. Don't miss out!</p>
-            <button className={styles.offerButton}>Shop Now</button>
-        </div>
-    )
+  return (
+    <div className={styles.offerContainer}>
+      <h1 className={styles.offerName}>Limited-Time Offer</h1>
+      <p className={styles.offerText}>
+        Save 20% on our Premium Selection of Single Origin Coffee Beans. Don't
+        miss out!
+      </p>
+      <Link to="/catalog" style={{color: 'inherit', textDecoration: 'none'}}>
+        <button className={styles.offerButton}>Shop Now</button>
+      </Link>
+    </div>
+  );
 }
