0fede79656a3c91c0a5c63767a82f5f38a587efd
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-04 17:18:16 +0300

    adding switch component from mui

--- client/src/admin/testTable.js
+++ client/src/admin/testTable.js
@@ -1,6 +1,70 @@
 import styles from "./testTable.module.css";
+import Switch, { SwitchProps } from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function TestTable({ data }) {
+
+    const IOSSwitch = styled((props: SwitchProps) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: 'rgba(255, 140, 58, 1);',
+        opacity: 1,
+        border: 0,
+        ...theme.applyStyles('dark', {
+          backgroundColor: 'rgba(255, 140, 58, 1);',
+        }),
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: 'rgba(255, 140, 58, 1);',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color: theme.palette.grey[100],
+      ...theme.applyStyles('dark', {
+        color: theme.palette.grey[600],
+      }),
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: 0.7,
+      ...theme.applyStyles('dark', {
+        opacity: 0.3,
+      }),
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: '#E9E9EA',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#39393D',
+    }),
+  },
+}));
+
   return (
     <div className={styles.tableWrap}>
       <div className={styles.tableData}>
@@ -31,7 +95,11 @@ export default function TestTable({ data }) {
               <td className={styles.tableBody}>{product.roastLevel}</td>
               <td className={styles.tableBody}>{product.origin}</td>
               <td className={styles.tableBody}>{product.qty}</td>
-              <td className={styles.tableBody}>{product.isActive}</td>
+              <td className={styles.tableBody}>
+                <FormControlLabel
+                    control={<IOSSwitch sx={{ m: 1 }} checked={product.isActive} />}
+                />
+                </td>
               <td className={styles.tableBody}>{product.bought}</td>
               <td className={styles.tableBody}>
                 <button>Edit</button>
