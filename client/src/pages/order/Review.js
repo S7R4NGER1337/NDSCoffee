879414f2e14335c33d358ca91e21446a3d97ecf9
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-05 17:50:14 +0300

    creating review page and rendering all items in the cart

--- client/src/pages/order/Review.js
+++ client/src/pages/order/Review.js
@@ -1,5 +1,61 @@
+import { useEffect } from "react";
+import styles from "./review.module.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import OrderCard from "./ProductCard";
+
+
 export default function Review() {
-    return (
-        <h1>Hello world</h1>
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const cart = JSON.parse(localStorage.getItem("cart"));
+
+  useEffect(() => {
+    if (state === null || cart === null) navigate("/");
+  }, [state, cart, navigate]);
+
+
+  async function submitOrder() {
+    const orderedProducts = JSON.parse(localStorage.getItem("cart"));
+    if (
+      state.name === "" ||
+      state.address === "" ||
+      state.phone === "" ||
+      orderedProducts === null
     )
+      return;
+    const bodyToSubmit = {
+      name: state.name,
+      address: state.address,
+      phone: state.phone,
+      productId: orderedProducts,
+    };
+
+    await fetch(`http://localhost:3030/products/order/s`, {
+      method: "POST",
+      body: JSON.stringify(bodyToSubmit),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    localStorage.removeItem("cart");
+    alert("Order successfull");
+    navigate("/");
+  }
+
+  return (
+    <div className={styles.reviewcontainer}>
+      <h1>Review Your Order</h1>
+      <div className={styles.cartContainer}>
+        <h1>Items</h1>
+        <div className={styles.itemsContainer}>
+            {cart === null ? (
+            <h1>There is nothing in the cart</h1>
+          ) : (
+            cart.map((item) => <OrderCard data={item} key={item.id} />)
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
