6b4c4fb59fb9d6694e2106a3d9ac4edc24e41dd0
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-05 18:20:02 +0300

    removing unused function

--- client/src/pages/order/Review.js
+++ client/src/pages/order/Review.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import styles from "./review.module.css";
 import { useLocation, useNavigate } from "react-router-dom";
-import OrderCard from "./ProductCard";
 import ReviewProduct from "./ReviewProduct";
 
 
@@ -15,35 +14,6 @@ export default function Review() {
   }, [state, cart, navigate]);
 
 
-  async function submitOrder() {
-    const orderedProducts = JSON.parse(localStorage.getItem("cart"));
-    if (
-      state.name === "" ||
-      state.address === "" ||
-      state.phone === "" ||
-      orderedProducts === null
-    )
-      return;
-    const bodyToSubmit = {
-      name: state.name,
-      address: state.address,
-      phone: state.phone,
-      productId: orderedProducts,
-    };
-
-    await fetch(`http://localhost:3030/products/order/s`, {
-      method: "POST",
-      body: JSON.stringify(bodyToSubmit),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-
-    localStorage.removeItem("cart");
-    alert("Order successfull");
-    navigate("/");
-  }
-
   return (
     <div className={styles.reviewcontainer}>
       <h1>Review Your Order</h1>
@@ -53,7 +23,6 @@ export default function Review() {
             {cart === null ? (
             <h1>There is nothing in the cart</h1>
           ) : (
-            // cart.map((item) => <OrderCard data={item} key={item.id}/>)
             cart.map((item) => <ReviewProduct data={item} key={item.id}/>)
           )}
         </div>
