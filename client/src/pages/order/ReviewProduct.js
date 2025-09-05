82a2bae0adba3be83efa76d8f8f4c6124f0c35dc
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-05 18:20:40 +0300

    adding basic styles to review product

--- client/src/pages/order/ReviewProduct.js
+++ client/src/pages/order/ReviewProduct.js
@@ -1,7 +1,7 @@
 import styles from "./reviewProduct.module.css";
 import { getProductDataById } from "../../api/products";
-import { useEffect, useState, useMemo } from "react";
-import {ProductImage} from '../../utils/imageHelper'
+import { useEffect, useState } from "react";
+import { ProductImage } from "../../utils/imageHelper";
 
 export default function ReviewProduct({ data }) {
   const [products, setProducts] = useState([]);
@@ -16,11 +16,9 @@ export default function ReviewProduct({ data }) {
 
   return (
     <div className={styles.productContainer}>
-      <ProductImage base64Data={products.image}/>
-      <div className={styles.productInfo}>
-        <h1>{products.name}</h1>
-        <p>{products.price}</p>
-      </div>
+      <ProductImage base64Data={products.image} />
+      <h1 className={styles.productName}>{products.name}</h1>
+      <p>$ {products.price}</p>
     </div>
   );
 }
