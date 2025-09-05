5377f726af5ffedb797f63025e2e0d5fc5cfd259
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-05 15:46:31 +0300

    adding new input fields

--- client/src/pages/order/Shipping.js
+++ client/src/pages/order/Shipping.js
@@ -5,24 +5,31 @@ import { useNavigate } from "react-router-dom";
 
 export default function Order() {
   const [userData, setUserData] = useState({
-    name: "",
-    address: "",
+    fullName: "",
+    streetAddres: "",
+    city: "",
+    postalCode: "",
     phone: "",
+    paymentType: "",
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const cart = JSON.parse(localStorage.getItem("cart"));
-
 
   async function submitOrder() {
     const orderedProducts = JSON.parse(localStorage.getItem("cart"));
-    if(userData.name === '' || userData.address === '' || userData.phone === '' || orderedProducts === null) return
+    if (
+      userData.name === "" ||
+      userData.address === "" ||
+      userData.phone === "" ||
+      orderedProducts === null
+    )
+      return;
     const bodyToSubmit = {
       name: userData.name,
       address: userData.address,
       phone: userData.phone,
       productId: orderedProducts,
     };
-
 
     await fetch(`http://localhost:3030/products/order/s`, {
       method: "POST",
@@ -32,60 +39,118 @@ export default function Order() {
       },
     });
 
-    localStorage.removeItem('cart')
-    alert('Order successfull')
-    navigate('/')
+    localStorage.removeItem("cart");
+    alert("Order successfull");
+    navigate("/");
   }
 
   return (
     <>
-    <div className={styles.orderContainer}>
-      <div className={styles.userData}>
-        <form className={styles.orderForm}>
-          <label> Name </label>
-          <input
-            required
-            type="text"
-            name="name"
-            value={userData.name}
-            onChange={(e) => setUserData({ ...userData, name: e.target.value })}
-          />
+      <div className={styles.orderContainer}>
+        <div className={styles.userData}>
+          <h1>Shipping Information</h1>
+          <form className={styles.orderForm}>
+            <div className={styles.orderFormGroup}>
+              <label> Full Name </label>
+              <input
+                placeholder="Enter your full name"
+                className={styles.orderFormInput}
+                required
+                type="text"
+                name="fullName"
+                value={userData.fullName}
+                onChange={(e) =>
+                  setUserData({ ...userData, fullName: e.target.value })
+                }
+              />
+            </div>
 
-          <label> Address </label>
-          <input
-            required
-            type="text"
-            name="address"
-            value={userData.address}
-            onChange={(e) =>
-              setUserData({ ...userData, address: e.target.value })
-            }
-            />
-
-          <label> Phone </label>
-          <input
-            required
-            type="text"
-            name="phone"
-            value={userData.phone}
-            onChange={(e) =>
-              setUserData({ ...userData, phone: e.target.value })
-            }
-            />
-
-          <button type="button" onClick={() => submitOrder()}>
-            ORDER NOW
-          </button>
-        </form>
+            <div className={styles.orderFormGroup}>
+              <label> Street Addres </label>
+              <input
+                placeholder="Enter your street address"
+                className={styles.orderFormInput}
+                required
+                type="text"
+                name="streetAddres"
+                value={userData.streetAddres}
+                onChange={(e) =>
+                  setUserData({ ...userData, streetAddres: e.target.value })
+                }
+              />
+            </div>
+            <div className={styles.orderFormGroup}>
+              <label> City </label>
+              <input
+                placeholder="Enter your city"
+                className={styles.orderFormInput}
+                required
+                type="text"
+                name="city"
+                value={userData.city}
+                onChange={(e) =>
+                  setUserData({ ...userData, city: e.target.value })
+                }
+              />
+            </div>
+            <div className={styles.orderFormGroup}>
+              <label> Phone </label>
+              <input
+                placeholder="Enter your phone number"
+                className={styles.orderFormInput}
+                required
+                type="number"
+                name="phone"
+                value={userData.phone}
+                onChange={(e) =>
+                  setUserData({ ...userData, phone: e.target.value })
+                }
+              />
+            </div>
+            <div className={styles.orderFormGroup}>
+              <label> Postal Code </label>
+              <input
+                placeholder="Enter your postal code"
+                className={styles.orderFormInput}
+                required
+                type="number"
+                name="phone"
+                value={userData.postalCode}
+                onChange={(e) =>
+                  setUserData({ ...userData, postalCode: e.target.value })
+                }
+              />
+            </div>
+            <div className={styles.orderFormGroup}>
+              <label> Payment Type </label>
+              <select
+                className={styles.orderFormInput}
+                name="paymentType"
+                value={userData.paymentType}
+                onChange={(e) =>
+                  setUserData({ ...userData, paymentType: e.target.value })
+                }
+              >
+                <option value="" disabled hidden>
+                  Choose Payment Type
+                </option>
+                <option value="cash">Cash on Delivery</option>
+                <option value="card">Credit/Debit Card</option>
+              </select>
+            </div>
+            <button type="button" onClick={() => submitOrder()}>
+              ORDER NOW
+            </button>
+          </form>
+        </div>
+        {/* <div className={styles.cart}>
+          {cart === null ? (
+            <h1>There is nothing in the cart</h1>
+          ) : (
+            cart.map((item) => <OrderCard data={item} key={item.id} />)
+          )}
+        </div> */}
       </div>
-      <div className={styles.cart}>
-        {cart === null ? <h1>There is nothing in the cart</h1>:
-        cart.map((item) => (
-          <OrderCard data={item} key={item.id} />
-        ))
-      }
-      </div>
-    </div>
-      </>
+    </>
   );
 }
