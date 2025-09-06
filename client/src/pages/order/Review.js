02cb70d16da5afe062309fd065af8d2983b29c00
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-06 15:09:22 +0300

    adding summary section

--- client/src/pages/order/Review.js
+++ client/src/pages/order/Review.js
@@ -23,26 +23,52 @@ export default function Review() {
           ) : (
             cart.map((item) => <ReviewProduct data={item} key={item.id} />)
           )}
-          <Link to="/cart" style={{textDecoration: 'none', color: 'rgba(255, 140, 58, 1)', alignSelf: 'end'}}>
+          <Link
+            to="/cart"
+            style={{
+              textDecoration: "none",
+              color: "rgba(255, 140, 58, 1)",
+              alignSelf: "end",
+            }}
+          >
             <p>Edit Your Items</p>
           </Link>
         </div>
       </div>
       <div className={styles.clientContainer}>
-          <div className={styles.clientDataContainer}>
-            <h1 className={styles.clientDataName}>Shipping Data</h1>
-            <div className={styles.clientData}>
-              <p>{state.fullName}</p>
-              <p>{state.phone}</p>
-              <p>{state.city}</p>
-              <p>{state.postalCode}</p>
-              <p>{state.streetAddres}</p>
-            </div>
+        <div className={styles.clientDataContainer}>
+          <h1 className={styles.clientDataName}>Shipping Data</h1>
+          <div className={styles.clientData}>
+            <p>{state.fullName}</p>
+            <p>{state.phone}</p>
+            <p>{state.city}</p>
+            <p>{state.postalCode}</p>
+            <p>{state.streetAddres}</p>
           </div>
-          <div className={styles.clientDataContainer}>
-            <h1 className={styles.clientDataName}>Payment Method</h1>
-            <p>{state.paymentType}</p>
+        </div>
+        <div className={styles.clientDataContainer}>
+          <h1 className={styles.clientDataName}>Payment Method</h1>
+          <p>
+            {state.paymentType === "cash" ? "Cash on Delivery" : "Paid by card"}
+          </p>
+        </div>
+      </div>
+      <div className={styles.orderSummaryContainer}>
+        <h1 className={styles.orderSummaryName}>Order Summary</h1>
+        <div className={styles.orderSummaryItems}>
+          <div className={styles.orderSummaryItem}>
+            <p className={styles.orderSummaryItemName}>Subtotal</p>
+            <p className={styles.orderSummaryItemPrice}>$ 12</p>
           </div>
+          <div className={styles.orderSummaryItem}>
+            <p className={styles.orderSummaryItemName}>Shipping</p>
+            <p className={styles.orderSummaryItemPrice}>$ 5</p>
+          </div>
+        </div>
+        <div className={styles.orderSummaryTotal}>
+          <h1 className={styles.orderSummaryTotalName}>Total</h1>
+          <p className={styles.orderSummaryTotalPrice}>$ 12</p>
+        </div>
       </div>
     </div>
   );
