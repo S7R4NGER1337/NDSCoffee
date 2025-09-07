eb64144c62c6678678ef94273bc92ad3d3e6d59e
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-07 17:44:36 +0300

    moving cart component in cart folder

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -1,9 +1,13 @@
 import styles from './cart.module.css'
 
 export default function Cart() {
+  const cart = JSON.parse(localStorage.getItem("cart"));
+
+  console.log(cart);
+  
     return(
         <div className={styles.cartContainer}>
-            <h1 className={styles.cartName}></h1>
+            <h1 className={styles.cartName}>Your Cart</h1>
             <div className={styles.cartDataContainer}>
                 <div className={styles.cartInfoContainer}>
 
