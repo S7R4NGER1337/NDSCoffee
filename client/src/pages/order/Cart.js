7c015081d283901fc335865163760901ac9d0b3c
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-07 17:43:05 +0300

    adding page skeleton

--- client/src/pages/order/Cart.js
+++ client/src/pages/order/Cart.js
@@ -1,5 +1,20 @@
 import styles from './cart.module.css'
 
 export default function Cart() {
-    
+    return(
+        <div className={styles.cartContainer}>
+            <h1 className={styles.cartName}></h1>
+            <div className={styles.cartDataContainer}>
+                <div className={styles.cartInfoContainer}>
+
+                </div>
+                <div className={styles.orderSummary}>
+
+                </div>
+            </div>
+            <div className={styles.alsoLike}>
+
+            </div>
+        </div>
+    )
 }
