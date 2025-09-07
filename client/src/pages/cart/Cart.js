5f397851ec8cb59feb5e428c4bb5ad7092ae2373
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-07 17:54:46 +0300

    getting cart products on load

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -1,24 +1,27 @@
-import styles from './cart.module.css'
+import styles from "./cart.module.css";
+import { getCartProducts } from "../../api/products";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
   const cart = JSON.parse(localStorage.getItem("cart"));
+  const [cartProducts, setCartProducts] = useState([]);
 
-  console.log(cart);
-  
-    return(
-        <div className={styles.cartContainer}>
-            <h1 className={styles.cartName}>Your Cart</h1>
-            <div className={styles.cartDataContainer}>
-                <div className={styles.cartInfoContainer}>
+  useEffect(() => {
+    async function name() {
+      const products = await getCartProducts(cart);
+      setCartProducts(products);
+    }
+    name();
+  }, [cart]);
 
-                </div>
-                <div className={styles.orderSummary}>
-
-                </div>
-            </div>
-            <div className={styles.alsoLike}>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.cartContainer}>
+      <h1 className={styles.cartName}>Your Cart</h1>
+      <div className={styles.cartDataContainer}>
+        <div className={styles.cartInfoContainer}></div>
+        <div className={styles.orderSummary}></div>
+      </div>
+      <div className={styles.alsoLike}></div>
+    </div>
+  );
 }
