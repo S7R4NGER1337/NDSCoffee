8d96fbc9d99d9a993319f1ffae58a745388a6817
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-07 17:24:19 +0300

    styling product page and adding qty input

--- client/src/pages/product/ProductPage.js
+++ client/src/pages/product/ProductPage.js
@@ -6,9 +6,11 @@ import { useNavigate } from "react-router";
 import { getProductDataById } from "../../api/products";
 
 export default function ProductPage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [productData, setProductData] = useState({});
+  const [qty, setQty] = useState(1);
   const productId = useLocation().pathname.split("/")[2];
+  const [qtyStatus, setQtyStatus] = useState(true);
 
   useEffect(() => {
     async function setData() {
@@ -16,6 +18,17 @@ export default function ProductPage() {
     }
     setData();
   }, [productId]);
+
+  function valueOnChange(e) {
+    const newQty = Number(e.target.value);
+    setQty(newQty);
+
+    if (newQty >= productData.qty || newQty <= 0) {
+      setQtyStatus(false);
+      return;
+    }
+    setQtyStatus(true);
+  }
 
   return (
     <>
@@ -27,12 +40,46 @@ export default function ProductPage() {
         />
         <div className={styles.productData}>
           <h1 className={styles.productName}>{productData.name}</h1>
-          <p className={styles.productPrice}>{productData.price} лв</p>
-          <p className={styles.productDescription}>{productData.description}</p>
-          <button className={styles.productBuy} onClick={() => {
-            setOrder(productId)
-            navigate('/order')
-            }}>Add to cart</button>
+          <div className={styles.productInfo}>
+            <p className={styles.productDescription}>
+              {productData.description}
+            </p>
+            <div className={styles.productpriceContainer}>
+              <p className={styles.productPriceName}>Price</p>
+              <p className={styles.productPrice}>${productData.price}</p>
+            </div>
+            <div className={styles.buttonsContainer}>
+              <div className={styles.qtyContainer}>
+                <label className={styles.qtyName}>Quantity</label>
+                {!qtyStatus && (
+                  <p
+                    style={{
+                      textAlign: "start",
+                      padding: "0.5em 0em",
+                      color: "red",
+                    }}
+                  >
+                    Not enough products
+                  </p>
+                )}
+                <input
+                  className={styles.qty}
+                  type="number"
+                  value={qty}
+                  onChange={(e) => valueOnChange(e)}
+                />
+              </div>
+              <button
+                className={styles.productBuy}
+                onClick={() => {
+                  setOrder(productId);
+                  navigate("/order");
+                }}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
