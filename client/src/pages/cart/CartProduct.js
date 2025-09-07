e10a63ddab76f28ef0a418caa732359cb5687632
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-08 02:42:55 +0300

    adding on click on every button that is connected to product price

--- client/src/pages/cart/CartProduct.js
+++ client/src/pages/cart/CartProduct.js
@@ -1,6 +1,16 @@
 import styles from "./cartProduct.module.css";
+import { useEffect, useState } from "react";
+import { methodsQty } from "../../utils/cart";
 
 export default function CartProduct({ productData }) {
+  const [qty, setQty] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    setQty(productData.qty);
+    setPrice(productData.price * qty);
+  }, [productData.price, productData.qty, qty]);
+
   return (
     <div className={styles.productContainer}>
       <img
@@ -11,15 +21,26 @@ export default function CartProduct({ productData }) {
       <div className={styles.productDataContainer}>
         <div className={styles.productInfoContainer}>
           <h1 className={styles.productInfoName}>{productData.name}</h1>
-          <p className={styles.productInfoPrice}>${productData.price}</p>
+          <p className={styles.productInfoPrice}>${price}</p>
         </div>
         <div className={styles.productActions}>
           <div className={styles.qtyEdit}>
-            <p className={styles.qtyAction}>-</p>
-            <p className={styles.qtyValue}>{productData.qty}</p>
-            <p className={styles.qtyAction}>+</p>
+            <p
+              onClick={() => methodsQty(productData._id, "sub", setQty)}
+              className={styles.qtyAction}
+            >
+              -
+            </p>
+            <p className={styles.qtyValue}>{qty}</p>
+            <p
+              onClick={() => methodsQty(productData._id, "sum", setQty)}
+              className={styles.qtyAction}
+            >
+              +
+            </p>
           </div>
           <img
+            onClick={() => methodsQty(productData._id, "del", setQty)}
             className={styles.trashIcon}
             src="/trash-solid-full-gray.svg"
             alt="trashIcon"
